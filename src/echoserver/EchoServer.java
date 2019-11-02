d36881e46cafbabe74286589a20666ca2cd2aec3
Paul Friederichsen <floogulinc@gmail.com>
2019-11-01 20:42:02 -0500

    Implement threaded EchoServer

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,11 +5,13 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer {
 	
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
+	public static final int PORT_NUMBER = 6013; 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
@@ -17,15 +19,35 @@ public class EchoServer {
 
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+
+		// Use number of CPU threads for thread pool
+		ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
 		while (true) {
 			Socket socket = serverSocket.accept();
+			executor.submit(() -> {
+				try {
+					InputStream input = socket.getInputStream();
+					OutputStream output = socket.getOutputStream(); 
+					
+					int read; // The byte we will be reading
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
+					// While there is still input to read
+					while ((read = input.read()) != -1)
+						output.write(read); // Write the input from the client back to the client
+					
+					output.flush(); // Flush the output stream
+
+					// Shutdown and close the connection to the client
+					socket.shutdownOutput();
+					socket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                
+			});
+			
 		}
 	}
 }
